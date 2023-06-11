bbe18a4c251332b92d87677ba49bf8faa4a5e548
archibald.perez <archieperez99@gmail.com>
2023-06-11 14:26:37 +0200

    Init vue project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <MainView />
+  <div
+    v-if="error"
+    class="box error"
+  >
+    An error occurred: {{ error.message }}
+  </div>
+</template>
+
+<script>
+import MainView from './components/MainView.vue'
+
+export default {
+  name: 'App',
+  expose: [],
+  components: { MainView },
+  data: () => ({
+    error: null,
+  }),
+
+  errorCaptured (errorCaptured) {
+    this.error = errorCaptured
+  },
+}
+</script>
---
+++ src/components/MainView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex">
+    <img
+      src="../../old/BoxyBots_test_v2.svg"
+    />
+  </div>
+</template>
